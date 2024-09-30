6dea1c74696b9a5ae34e1683ee24873c40a9b3a3
Keonyoung <joseph12752@gmail.com>
2024-09-30 16:33:45 +0900

    port 80

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const fs = require('fs').promises;
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 4000;
+const port = 80;
 
 // CORS 설정
 app.use(cors({
